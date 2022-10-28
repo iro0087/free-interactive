3e7f860c2d2f64a2a5e5a5ac8ffc02ecc0b42316
iro0087 <114911243+iro0087@users.noreply.github.com>
2022-10-28 14:46:22 +0200

    Update plot.py

--- plot.py
+++ plot.py
@@ -204,11 +204,11 @@ def fun():
 
         else:
 
-            r_max1 = len(lx) 
+            r_max1 = len(lx) - 1
 
     else:
 
-        r_max2 = len(ly)
+        r_max2 = len(ly) - 1
 
         if len(xmax.get()) > 0:
 
@@ -216,7 +216,7 @@ def fun():
 
         else:
 
-            r_max1 = len(lx)
+            r_max1 = len(lx) - 1
 
     img = plt.imread(fichier)
 
